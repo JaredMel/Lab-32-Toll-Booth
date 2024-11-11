970d6a64a0f92a0cb5f0286ae338e31406990a5c
JaredMel <jareddmelendez1764@gmail.com>
2024-11-11 09:19:08 -0800

    update 2

--- main.cpp
+++ main.cpp
@@ -14,13 +14,30 @@ int main()
     for (size_t i = 0; i < SIZE; i++)
     {
         line.push_back(Car());
-        cout << "[" << line.back().getYear() << " " << line.back().getMake() << " (" << line.back().getTransponder() << ")]" << endl;
+        cout << "   [" << line.back().getYear() << " " << line.back().getMake() << " (" << line.back().getTransponder() << ")]" << endl;
     }
     cout << endl;
     
     for (size_t i = 0; i < CYCLES; i++)
     {
+        int prob = rand() % 100 + 1;
         cout << "Time: " << i+1 << " Operation: ";
-        
+
+        if (prob <= 55)
+        {
+            line.push_back(Car());
+            cout << "Joined lane: [" << line.back().getYear() << " " << line.back().getMake() << " (" << line.back().getTransponder() << ")]" << endl;
+        }
+        else if (prob <= 45)
+        {
+            cout << "Car paid: [" << line.front().getYear() << " " << line.front().getMake() << " (" << line.front().getTransponder() << ")]" << endl;
+            line.pop_front();
+        }
+
+        cout << "Queue:" << endl;
+        for (size_t i = 0; i < line.size(); i++)
+        {
+            cout << "   " << line[i].print() << endl;
+        }
     }
 }
