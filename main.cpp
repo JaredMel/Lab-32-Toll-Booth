21113bcc4607d59de77b58098b52c059df2e5d32
JaredMel <jareddmelendez1764@gmail.com>
2024-11-11 09:55:38 -0800

    update 1

--- main.cpp
+++ main.cpp
@@ -1,43 +1,56 @@
 #include "Car.h"
 #include <iostream>
 #include <deque>
+#include <array>
 using namespace std;
 
 const int SIZE = 2;
+const int CYCLES = 20;
+const int NUM_LANES = 4;
 
 int main()
 {
     deque<Car> line;
+    array<deque<Car>, NUM_LANES> lanes;
     int count = 0;
+    int prob;
 
     cout << "Initial queue :" << endl;
-    for (size_t i = 0; i < SIZE; i++)
+    for (size_t i = 0; i < NUM_LANES; i++)
     {
-        line.push_back(Car());
-        cout << "   ";
-        line.back().print();
+        cout << "Lane " << i+1 << ":" << endl;
+        for (size_t i = 0; i < SIZE; i++)
+        {
+            lanes[i].push_back(Car());
+            cout << "   ";
+            lanes[i].back().print();
+        }
     }
     cout << endl;
     
-    while(!line.empty())
+    while(count < CYCLES)
     {
         count++;
-        int prob = rand() % 100 + 1;
         cout << "Time: " << count << endl;
 
-        if (prob <= 45)
+        for (size_t i = 0; i < NUM_LANES; i++)
         {
-            cout << "Operation: Car paid: ";
-            line.front().print();
-            line.pop_front();
             prob = rand() % 100 + 1;
-        }
-        if (prob <= 55)
-        {
-            line.push_back(Car());
-            cout << "Operation: Joined lane: ";
-            line.back().print();
-            prob = rand() % 100 + 1;
+
+            if (prob <= 45)
+            {
+                cout << "Lane: " << i+1 << " Paid: ";
+                lanes[i].front().print();
+                lanes[i].pop_front();
+                prob = rand() % 100 + 1;
+            }
+            if (prob <= 55)
+            {
+                lanes[i].push_back(Car());
+                cout << "Lane: " << i+1 << "Joined: ";
+                lanes[i].back().print();
+                prob = rand() % 100 + 1;
+            }
         }
 
         cout << "Queue:" << endl;
